5e4b8d41ae0b193db335db6ce8f9e91e1b8ae61b
Vikkram SL <vikkram014@gmail.com>
2025-05-23 10:31:21 +0530

    implemented forget password

--- lib/features/authentication/presentation/signin_page.dart
+++ lib/features/authentication/presentation/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vertex_app/features/authentication/presentation/screens/forgot_password.dart';
 import '../auth_manager.dart';
 import 'signup_page.dart';
 import 'widgets/auth_button.dart';
@@ -161,12 +162,16 @@ class _SignInPageState extends State<SignInPage> {
                         child: TextButton(
                           onPressed: () {
                             // Implement forgot password
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Password reset will be implemented soon'),
-                                backgroundColor: VertexColors.deepSapphire,
-                              ),
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ForgotPassword()),
                             );
+                            // ScaffoldMessenger.of(context).showSnackBar(
+                            //   const SnackBar(
+                            //     content: Text('Password reset will be implemented soon'),
+                            //     backgroundColor: VertexColors.deepSapphire,
+                            //   ),
+                            // );
                           },
                           child: Text(
                             'Forgot Password?',
